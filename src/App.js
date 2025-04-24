17364719df74747bd053e434f7c7045b56a8a681
Ricquel215 <ricqueljh215@gmail.com>
2025-04-24 16:34:29 -0500

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Ricquel Harper and is{" "}
+        <a
+          href="https://github.com/Ricquel215/rh-react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
